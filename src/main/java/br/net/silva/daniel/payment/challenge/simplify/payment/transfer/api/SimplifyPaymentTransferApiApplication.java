0b67166ef00ef928cedc482e7248812a5a0c6ef0
silva01 <silva20102@gmail.com>
2024-06-28 05:42:56 -0300

    build: #8 Initial Commit

---
+++ src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/SimplifyPaymentTransferApiApplication.java
@@ -0,0 +1,13 @@
+package br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifyPaymentTransferApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplifyPaymentTransferApiApplication.class, args);
+    }
+
+}
