cdcaa2248ff8a83a9eab098043955655180ed94b
silva01 <silva20102@gmail.com>
2024-07-08 06:56:35 -0300

    refactor: #4 Added attribute not blank

--- src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/controller/request/BaseRequest.java
+++ src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/controller/request/BaseRequest.java
@@ -1,12 +1,19 @@
 package br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.controller.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
 public abstract class BaseRequest {
+
+    @NotBlank(message = "Attribute email is required")
     private final String email;
+
+    @NotBlank(message = "Attribute name is required")
     private final String name;
+
+    @NotBlank(message = "Attribute password is required")
     private final String password;
 }
--- src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/controller/request/ClientRequest.java
+++ src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/controller/request/ClientRequest.java
@@ -1,10 +1,12 @@
 package br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.controller.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class ClientRequest extends BaseRequest {
 
+    @NotBlank(message = "Attribute identify is required")
     private final String identify;
 
     public ClientRequest(String email, String name, String password, String identify) {
