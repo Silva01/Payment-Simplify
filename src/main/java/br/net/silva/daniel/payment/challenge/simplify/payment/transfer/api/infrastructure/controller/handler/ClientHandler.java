07fbe567ae2fb095bdf0bfb6fbb8d1163798b56a
silva01 <silva20102@gmail.com>
2024-07-08 07:20:34 -0300

    feat: #4 Added new handler for help when throw exception ClientAlreadyExistsException

--- src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/controller/handler/ClientHandler.java
+++ src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/controller/handler/ClientHandler.java
@@ -1,11 +1,13 @@
 package br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.controller.handler;
 
+import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.client.exception.ClientAlreadyExistsException;
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.controller.response.ResponseError;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -17,5 +19,11 @@ public class ClientHandler extends ResponseEntityExceptionHandler {
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
         final var response = new ResponseError(HttpStatus.NOT_ACCEPTABLE.value(), "Invalid Request: Request has fields with value empty or null");
         return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(response);
+    }
+
+    @ExceptionHandler(ClientAlreadyExistsException.class)
+    public ResponseEntity<Object> handleClientAlreadyExistsException(ClientAlreadyExistsException ex) {
+        final var response = new ResponseError(HttpStatus.CONFLICT.value(), ex.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
     }
 }
