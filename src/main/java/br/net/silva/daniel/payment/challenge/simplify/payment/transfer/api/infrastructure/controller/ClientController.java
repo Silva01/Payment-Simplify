564078109017b9019a7a5d7e554013fef103c3da
silva01 <silva20102@gmail.com>
2024-07-08 06:21:03 -0300

    feat: #4 Added controller for client

---
+++ src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/controller/ClientController.java
@@ -0,0 +1,20 @@
+package br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.controller;
+
+import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.controller.request.ClientRequest;
+import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.controller.response.ClientResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/clients")
+public class ClientController {
+
+    @PostMapping
+    public ResponseEntity<ClientResponse> createClient(@RequestBody ClientRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ClientResponse(1L, request.getCpf()));
+    }
+}
