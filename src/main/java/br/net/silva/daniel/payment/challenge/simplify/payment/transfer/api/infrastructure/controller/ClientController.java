ed3cd986d14022f624071738db754b23b2b4c94c
silva01 <silva20102@gmail.com>
2024-07-08 07:19:59 -0300

    feat: #4 Refactored for use ClientService class

--- src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/controller/ClientController.java
+++ src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/controller/ClientController.java
@@ -1,8 +1,11 @@
 package br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.controller;
 
+import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.client.exception.ClientAlreadyExistsException;
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.controller.request.ClientRequest;
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.controller.response.ClientResponse;
+import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.service.ClientService;
 import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,10 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/clients")
+@AllArgsConstructor
 public class ClientController {
 
+    private final ClientService service;
+
     @PostMapping
-    public ResponseEntity<ClientResponse> createClient(@RequestBody @Valid ClientRequest request) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(new ClientResponse(1L, request.getIdentify()));
+    public ResponseEntity<ClientResponse> createClient(@RequestBody @Valid ClientRequest request) throws ClientAlreadyExistsException {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.create(request));
     }
 }
