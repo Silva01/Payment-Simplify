6726bc8ab6a29c626e392f77c23f61a7107a0c7f
silva01 <silva20102@gmail.com>
2024-07-08 06:58:24 -0300

    test: #4 Added test when create client to flux createClient_WithInvalidData_ReturnsError406

--- src/test/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/controller/ClientControllerTest.java
+++ src/test/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/controller/ClientControllerTest.java
@@ -3,9 +3,13 @@ package br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infr
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.controller.request.ClientRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.stream.Stream;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -35,5 +39,29 @@ class ClientControllerTest {
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.id").isNumber())
                 .andExpect(jsonPath("$.identify").value(request.getIdentify()));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideInvalidData")
+    void createClient_WithInvalidData_ReturnsError406(ClientRequest request) throws Exception {
+        mockMvc.perform(post("/clients")
+                                .contentType("application/json")
+                                .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isNotAcceptable())
+                .andExpect(jsonPath("$.code").value(406))
+                .andExpect(jsonPath("$.message").value("Invalid Request: Request has fields with value empty or null"));
+    }
+
+    private static Stream<ClientRequest> provideInvalidData() {
+        return Stream.of(
+                new ClientRequest("", "test", "1234566", "99988877766"),
+                new ClientRequest(null, "test", "1234566", "99988877766"),
+                new ClientRequest("test@test.com", "test", "", "99988877766"),
+                new ClientRequest("test@test.com", "", "1234566", "99988877766"),
+                new ClientRequest("test@test.com", null, "1234566", "99988877766"),
+                new ClientRequest("test@test.com", "test", null, "99988877766"),
+                new ClientRequest("test@test.com", "test", "1234566", ""),
+                new ClientRequest("test@test.com", "test", "1234566", null)
+        );
     }
 }
