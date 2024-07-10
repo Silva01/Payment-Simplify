ad2b99ec27a5d0bd0590814ab210383e4524e41e
silva01 <silva20102@gmail.com>
2024-07-10 06:35:10 -0300

    refactor: #4 Refactored for save client in database

--- src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/service/ClientService.java
+++ src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/service/ClientService.java
@@ -3,11 +3,31 @@ package br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infr
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.client.exception.ClientAlreadyExistsException;
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.controller.request.ClientRequest;
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.controller.response.ClientResponse;
+import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.entity.Client;
+import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.entity.ClientDetails;
+import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.entity.repository.ClientRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClientService {
+
+    private final ClientRepository repository;
+
+    public ClientService(ClientRepository repository) {
+        this.repository = repository;
+    }
+
     public ClientResponse create(ClientRequest request) throws ClientAlreadyExistsException {
-        return null;
+        final var newClient = new Client();
+        newClient.setCpf(request.getIdentify());
+
+        final var details = new ClientDetails();
+        details.setEmail(request.getEmail());
+        details.setName(request.getName());
+
+        newClient.setDetails(details);
+
+        repository.save(newClient);
+        return new ClientResponse(0L, newClient.getCpf());
     }
 }
