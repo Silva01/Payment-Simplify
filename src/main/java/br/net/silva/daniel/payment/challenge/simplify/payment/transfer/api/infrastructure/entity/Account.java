c9452b2ac65405401abe9926a948db7fd53b44cc
silva01 <silva20102@gmail.com>
2024-07-10 07:11:52 -0300

    feat: #4 Added entity for account

---
+++ src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/entity/Account.java
@@ -0,0 +1,34 @@
+package br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.entity;
+
+import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.client.enuns.AccountType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "account")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private AccountType type;
+    private BigDecimal balance;
+    private String password;
+
+    @Column(name = "client_identity")
+    private String clientIdentity;
+}
