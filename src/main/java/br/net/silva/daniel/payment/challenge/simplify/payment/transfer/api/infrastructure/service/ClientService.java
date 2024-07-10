5ab17a500770c94968464b68c9d9a3010e1fb613
silva01 <silva20102@gmail.com>
2024-07-10 07:13:11 -0300

    refactor: #4 Added building to data account

--- src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/service/ClientService.java
+++ src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/service/ClientService.java
@@ -1,12 +1,16 @@
 package br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.service;
 
+import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.client.enuns.AccountType;
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.client.exception.ClientAlreadyExistsException;
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.controller.request.ClientRequest;
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.controller.response.ClientResponse;
+import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.entity.Account;
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.entity.Client;
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.entity.ClientDetails;
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.entity.repository.ClientRepository;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 
 @Service
 public class ClientService {
@@ -24,6 +28,12 @@ public class ClientService {
         final var details = new ClientDetails();
         details.setEmail(request.getEmail());
         details.setName(request.getName());
+
+        final var account = new Account();
+        account.setType(AccountType.CLIENT);
+        account.setBalance(BigDecimal.valueOf(1000));
+        account.setPassword(request.getPassword());
+        newClient.setAccount(account);
 
         newClient.setDetails(details);
 
