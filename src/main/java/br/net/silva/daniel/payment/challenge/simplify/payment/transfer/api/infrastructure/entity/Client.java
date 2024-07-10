bda82400da5635942318512f878e2b206ac79960
silva01 <silva20102@gmail.com>
2024-07-10 07:12:27 -0300

    refactor: #4 Refactored entity client to use account entity

--- src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/entity/Client.java
+++ src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/entity/Client.java
@@ -1,8 +1,11 @@
 package br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.SecondaryTable;
 import jakarta.persistence.Table;
@@ -22,4 +25,8 @@ public final class Client {
 
     @Embedded
     private ClientDetails details;
+
+    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(referencedColumnName = "client_identity")
+    private Account account;
 }
