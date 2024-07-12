6edce440b6b6f49428da0e8651eb72b3fcd05e70
silva01 <silva20102@gmail.com>
2024-07-12 06:44:03 -0300

    refactor: #4 Refactored name identify to cpf in client request object

--- src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/controller/request/ClientRequest.java
+++ src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/controller/request/ClientRequest.java
@@ -1,16 +1,25 @@
 package br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.controller.request;
 
+import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.client.enuns.AccountType;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Getter;
 
-@Getter
 public class ClientRequest extends BaseRequest {
 
     @NotBlank(message = "Attribute identify is required")
-    private final String identify;
+    private final String cpf;
 
-    public ClientRequest(String email, String name, String password, String identify) {
+    public ClientRequest(String email, String name, String password, String cpf) {
         super(email, name, password);
-        this.identify = identify;
+        this.cpf = cpf;
+    }
+
+    @Override
+    public AccountType type() {
+        return AccountType.CLIENT;
+    }
+
+    @Override
+    public String identify() {
+        return cpf;
     }
 }
