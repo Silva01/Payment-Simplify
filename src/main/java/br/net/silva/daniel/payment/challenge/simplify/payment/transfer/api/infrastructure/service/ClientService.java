f8b5687affe2cde6db4b5a8387c560b34a9c5ebd
silva01 <silva20102@gmail.com>
2024-07-12 05:59:04 -0300

    feat: #4 Added validation to when client exists with identify or email in database

--- src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/service/ClientService.java
+++ src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/service/ClientService.java
@@ -3,6 +3,7 @@ package br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infr
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.client.exception.ClientAlreadyExistsException;
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.controller.request.ClientRequest;
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.controller.response.ClientResponse;
+import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.entity.Client;
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.entity.repository.ClientRepository;
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.factory.ClientFactory;
 import org.springframework.stereotype.Service;
@@ -17,7 +18,22 @@ public class ClientService {
     }
 
     public ClientResponse create(ClientRequest request) throws ClientAlreadyExistsException {
+        validateIfAccountExists(request.getIdentify(), request.getEmail());
         final var newClient = repository.save(ClientFactory.createClient(request));
         return new ClientResponse(newClient.getAccount().getId(), newClient.getId());
+    }
+
+    public void validateIfAccountExists(String identify, String email) throws ClientAlreadyExistsException {
+        final var clientOpt = repository.findByIdOrEmail(identify, email);
+        if (clientOpt.isPresent()) {
+            throw new ClientAlreadyExistsException(createMessageError(identify, email, clientOpt.get()));
+        }
+    }
+
+    private String createMessageError(String identify, String email, Client client) {
+        if (client.getId().equals(identify))
+            return String.format("Invalid Request: Client with CPF/CNPJ %s already exists", identify);
+        else
+            return String.format("Invalid Request: Client with email %s already exists", email);
     }
 }
