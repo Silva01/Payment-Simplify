a81a6e781b67b15067e44e4a7ea3404b7edec2a6
silva01 <silva20102@gmail.com>
2024-07-12 07:21:00 -0300

    test: #4 Added test to service method to Shopkeeper

--- src/test/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/service/ClientServiceTest.java
+++ src/test/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/service/ClientServiceTest.java
@@ -3,6 +3,7 @@ package br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infr
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.client.enuns.AccountType;
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.client.exception.ClientAlreadyExistsException;
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.controller.request.ClientRequest;
+import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.controller.request.ShopkeeperRequest;
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.entity.Client;
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.entity.repository.ClientRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,6 +15,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import java.math.BigDecimal;
 
 import static br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.commons.ClientFakerBuilder.buildClientRequest;
+import static br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.commons.ClientFakerBuilder.buildShopkeeperRequest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
@@ -73,7 +75,7 @@ class ClientServiceTest {
         assertThatCode(() -> {
             service.create(request2);
         }).isInstanceOf(ClientAlreadyExistsException.class)
-                .hasMessage(String.format("Invalid Request: Client with CPF/CNPJ %s already exists", request2.identify()));
+                .hasMessage(String.format("Invalid Request: CPF/CNPJ %s already exists", request2.identify()));
     }
 
     @Test
@@ -93,6 +95,69 @@ class ClientServiceTest {
         assertThatCode(() -> {
             service.create(request2);
         }).isInstanceOf(ClientAlreadyExistsException.class)
-                .hasMessage(String.format("Invalid Request: Client with email %s already exists", request2.getEmail()));
+                .hasMessage(String.format("Invalid Request: email %s already exists", request2.getEmail()));
+    }
+
+    @Test
+    void createShopkeeper_WithValidData_CreateWithSuccess() throws ClientAlreadyExistsException {
+        final var request = buildShopkeeperRequest();
+
+        final var response = service.create(request);
+        assertThat(response).isNotNull();
+        assertThat(response.accountId()).isNotNull();
+        assertThat(response.identify()).isEqualTo(request.identify());
+
+        final var clientSut = entityManager.find(Client.class, response.identify());
+        assertThat(clientSut).isNotNull();
+        assertThat(clientSut.getId()).isEqualTo(request.identify());
+        assertThat(clientSut.getEmail()).isEqualTo(request.getEmail());
+        assertThat(clientSut.getName()).isEqualTo(request.getName());
+
+        final var accountSut = clientSut.getAccount();
+        assertThat(accountSut).isNotNull();
+        assertThat(accountSut.getId()).isNotNull();
+        assertThat(accountSut.getType()).isEqualTo(AccountType.SHOPKEEPER);
+        assertThat(accountSut.getBalance()).isEqualTo(BigDecimal.valueOf(1000));
+        assertThat(accountSut.getPassword()).isEqualTo(request.getPassword());
+    }
+
+    @Test
+    void createShopkeeper_WithUsedCpf_ReturnsException() {
+        final var request = buildShopkeeperRequest();
+        final var request2 = new ShopkeeperRequest(
+                "test@test.com",
+                "Test",
+                request.getPassword(),
+                request.identify()
+        );
+
+        assertThatCode(() -> {
+            service.create(request);
+        }).doesNotThrowAnyException();
+
+        assertThatCode(() -> {
+            service.create(request2);
+        }).isInstanceOf(ClientAlreadyExistsException.class)
+                .hasMessage(String.format("Invalid Request: CPF/CNPJ %s already exists", request2.identify()));
+    }
+
+    @Test
+    void createShopkeeper_WithUsedEmail_ReturnsException() {
+        final var request = buildShopkeeperRequest();
+        final var request2 = new ShopkeeperRequest(
+                request.getEmail(),
+                "Test",
+                request.getPassword(),
+                "00000000099/0001-99"
+        );
+
+        assertThatCode(() -> {
+            service.create(request);
+        }).doesNotThrowAnyException();
+
+        assertThatCode(() -> {
+            service.create(request2);
+        }).isInstanceOf(ClientAlreadyExistsException.class)
+                .hasMessage(String.format("Invalid Request: email %s already exists", request2.getEmail()));
     }
 }
