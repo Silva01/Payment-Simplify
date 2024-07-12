2486d72102e2f2fc8d5b4f94981ec0560daebb01
silva01 <silva20102@gmail.com>
2024-07-12 05:58:17 -0300

    feat: #4 Added method to find client by cpf/cnpj or email

--- src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/entity/repository/ClientRepository.java
+++ src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/entity/repository/ClientRepository.java
@@ -3,5 +3,8 @@ package br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infr
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.entity.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ClientRepository extends JpaRepository<Client, String>{
+    Optional<Client> findByIdOrEmail(String id, String email);
 }
