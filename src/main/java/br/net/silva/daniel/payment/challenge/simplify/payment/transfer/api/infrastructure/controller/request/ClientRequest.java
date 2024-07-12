1fe1b7aadc16d4e34acf0424acb29a11d4828860
silva01 <silva20102@gmail.com>
2024-07-12 07:33:36 -0300

    refactor: #4 Removed imports not used

--- src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/controller/request/ClientRequest.java
+++ src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/controller/request/ClientRequest.java
@@ -1,7 +1,6 @@
 package br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.controller.request;
 
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.client.enuns.AccountType;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
