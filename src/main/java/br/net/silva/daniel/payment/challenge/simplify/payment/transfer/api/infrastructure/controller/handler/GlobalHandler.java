df1726d1067fb2f3d58b67bddd950f27f1250e0b
silva01 <silva20102@gmail.com>
2024-07-16 06:30:36 -0300

    refactor: #6 Added new handler to AccountWithoutBalanceException

--- src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/controller/handler/GlobalHandler.java
+++ src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/controller/handler/GlobalHandler.java
@@ -2,6 +2,7 @@ package br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infr
 
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.client.exception.ClientAlreadyExistsException;
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.transfer.exception.AccountNotFoundException;
+import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.transfer.exception.AccountWithoutBalanceException;
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.controller.response.ResponseError;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -32,5 +33,11 @@ public class GlobalHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity<Object> handleAccountNotFoundException(AccountNotFoundException ex) {
         final var response = new ResponseError(HttpStatus.NOT_FOUND.value(), ex.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
+    @ExceptionHandler(AccountWithoutBalanceException.class)
+    public ResponseEntity<Object> handleAccountWithoutBalanceException(AccountWithoutBalanceException ex) {
+        final var response = new ResponseError(HttpStatus.FORBIDDEN.value(), ex.getMessage());
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(response);
     }
 }
