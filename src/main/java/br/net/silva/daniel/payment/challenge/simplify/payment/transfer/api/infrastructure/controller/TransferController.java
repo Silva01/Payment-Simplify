4066d3218e812b222e5a5723a972e5a89eeb6ec9
silva01 <silva20102@gmail.com>
2024-07-16 06:03:39 -0300

    feat: #6 Added endpoint to transfer values

--- src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/controller/TransferController.java
+++ src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/controller/TransferController.java
@@ -1,4 +1,19 @@
 package br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.controller;
 
+import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.controller.response.TransferResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/transfer")
 public class TransferController {
+
+    @PostMapping
+    public ResponseEntity<TransferResponse> transfer() {
+        return ResponseEntity.ok(new TransferResponse(UUID.randomUUID().toString()));
+    }
 }
