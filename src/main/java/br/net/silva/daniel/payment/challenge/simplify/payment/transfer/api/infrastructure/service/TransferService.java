735e1be04e150815cb566a8f63fa1d4554c445b4
silva01 <silva20102@gmail.com>
2024-07-17 07:08:11 -0300

    refactor: #6 Refactored to save transaction of transfer

--- src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/service/TransferService.java
+++ src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/service/TransferService.java
@@ -1,14 +1,24 @@
 package br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.service;
 
+import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.shared.interfaces.Factory;
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.controller.request.TransferRequest;
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.controller.response.TransferResponse;
+import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.entity.Transfer;
+import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.entity.repository.TransferRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.UUID;
-
 @Service
+@AllArgsConstructor
 public class TransferService {
+
+    private final TransferRepository repository;
+    private Factory<Transfer, TransferRequest> factory;
+
     public TransferResponse transfer(TransferRequest request) throws Exception {
-        return new TransferResponse(UUID.randomUUID().toString());
+        final var transfer = factory.convertToEntityFrom(request);
+        final var transferSaved = repository.save(transfer);
+
+        return new TransferResponse(transferSaved.getId().toString());
     }
 }
