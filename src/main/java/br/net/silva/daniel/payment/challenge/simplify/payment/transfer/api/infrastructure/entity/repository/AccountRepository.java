4e65b15336777360c6efa1fde25115bdca7ee82a
silva01 <silva20102@gmail.com>
2024-07-17 06:49:03 -0300

    refactor: #6 refactored type interface by string to long

--- src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/entity/repository/AccountRepository.java
+++ src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/entity/repository/AccountRepository.java
@@ -3,5 +3,5 @@ package br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infr
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AccountRepository extends JpaRepository<Account, String>{
+public interface AccountRepository extends JpaRepository<Account, Long>{
 }
