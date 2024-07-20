cc8934fc73df54a8b4294f52a182b69d6a4f70e6
silva01 <silva20102@gmail.com>
2024-07-20 07:24:47 -0300

    feat: #6 Added validation method to id allow transfer from source

--- src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/service/TransferService.java
+++ src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/service/TransferService.java
@@ -1,6 +1,8 @@
 package br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.service;
 
+import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.client.enuns.AccountType;
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.shared.interfaces.Factory;
+import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.transfer.exception.AccountNotAllowTransactionException;
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.controller.request.TransferRequestTransaction;
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.controller.response.TransferResponse;
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.entity.Transfer;
@@ -17,8 +19,16 @@ public class TransferService {
 
     public TransferResponse transfer(TransferRequestTransaction request) throws Exception {
         final var transfer = factory.convertToEntityFrom(request);
+        validateIfAllowTransfer(transfer);
+
         final var transferSaved = repository.save(transfer);
 
         return new TransferResponse(transferSaved.getId().toString());
+    }
+
+    public void validateIfAllowTransfer(Transfer request) throws AccountNotAllowTransactionException {
+        if (AccountType.SHOPKEEPER.equals(request.getSender().getType())) {
+            throw new AccountNotAllowTransactionException("Account with id %d not allow make transfer".formatted(request.getSender().getId()));
+        }
     }
 }
