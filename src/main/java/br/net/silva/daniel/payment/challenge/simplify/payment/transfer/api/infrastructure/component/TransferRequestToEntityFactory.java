c549cbd4a40c3d51d4c53a480c535a74fbfd4f85
silva01 <silva20102@gmail.com>
2024-07-20 06:49:23 -0300

    refactor: #6 Refactored throws exception

--- src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/component/TransferRequestToEntityFactory.java
+++ src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/component/TransferRequestToEntityFactory.java
@@ -1,7 +1,8 @@
 package br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.component;
 
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.shared.interfaces.Factory;
-import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.controller.request.TransferRequest;
+import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.transfer.exception.AccountNotFoundException;
+import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.controller.request.TransferRequestTransaction;
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.entity.Transfer;
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.entity.repository.AccountRepository;
 import lombok.AllArgsConstructor;
@@ -11,18 +12,18 @@ import java.util.UUID;
 
 @Component
 @AllArgsConstructor
-public class TransferRequestToEntityFactory implements Factory<Transfer, TransferRequest> {
+public class TransferRequestToEntityFactory implements Factory<Transfer, TransferRequestTransaction> {
 
     private final AccountRepository accountRepository;
 
     @Override
-    public Transfer convertToEntityFrom(TransferRequest transferRequest) {
+    public Transfer convertToEntityFrom(TransferRequestTransaction transferRequest) throws AccountNotFoundException {
         return Transfer
                 .builder()
                 .id(UUID.randomUUID())
                 .value(transferRequest.value())
-                .sender(accountRepository.findById(transferRequest.payer()).orElseThrow())
-                .receiver(accountRepository.findById(transferRequest.payee()).orElseThrow())
+                .sender(accountRepository.findById(transferRequest.payer()).orElseThrow(() -> new AccountNotFoundException(String.format("Account with id %d not found", transferRequest.payer()))))
+                .receiver(accountRepository.findById(transferRequest.payee()).orElseThrow(() -> new AccountNotFoundException(String.format("Account with id %d not found", transferRequest.payee()))))
                 .build();
     }
 }
