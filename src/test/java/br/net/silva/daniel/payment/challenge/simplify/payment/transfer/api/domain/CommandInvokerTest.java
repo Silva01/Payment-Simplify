4ede87624df943f322965736b6f76ba9d20df94b
silva01 <silva20102@gmail.com>
2024-10-19 20:12:26 -0300

    refactor: Refactored to use new record and test new record

--- src/test/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/domain/CommandInvokerTest.java
+++ src/test/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/domain/CommandInvokerTest.java
@@ -3,6 +3,8 @@ package br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.doma
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.transfer.BadTransferException;
 import org.junit.jupiter.api.Test;
 
+import static br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.CommandResponseFluxEnum.EXTRACT;
+import static br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.CommandResponseFluxEnum.TRANSFER;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -14,7 +16,7 @@ class CommandInvokerTest {
         final var command = new Command() {
             @Override
             public CommandResponse execute() {
-                return new CommandResponse("Hello World!");
+                return new CommandResponse("Hello World!", TRANSFER);
             }
         };
 
@@ -24,8 +26,38 @@ class CommandInvokerTest {
 
         // Assert
         assertThat(result).isNotNull();
-        assertThat(result).hasSize(1);
-        assertThat(result.get(0).response(String.class)).isEqualTo("Hello World!");
+        assertThat(result.responses()).hasSize(1);
+        assertThat(result.getResponseForFlux(TRANSFER).response(String.class)).isEqualTo("Hello World!");
+    }
+
+    @Test
+    void executionCommand_ProcessWitmMoreTwoResponse_WithoutErrors() throws BadTransferException {
+        // Arrange
+        final var command1 = new Command() {
+            @Override
+            public CommandResponse execute() {
+                return new CommandResponse("Hello World!", TRANSFER);
+            }
+        };
+
+        final var command2 = new Command() {
+            @Override
+            public CommandResponse execute() {
+                return new CommandResponse(999, EXTRACT);
+            }
+        };
+
+        final var commandInvoker = new CommandInvoker();
+        commandInvoker.add(command1);
+        commandInvoker.add(command2);
+        // Act
+        final var result = commandInvoker.start();
+
+        // Assert
+        assertThat(result).isNotNull();
+        assertThat(result.responses()).hasSize(2);
+        assertThat(result.getResponseForFlux(TRANSFER).response(String.class)).isEqualTo("Hello World!");
+        assertThat(result.getResponseForFlux(EXTRACT).response(Integer.class)).isEqualTo(999);
     }
 
     @Test
@@ -35,7 +67,7 @@ class CommandInvokerTest {
             @Override
             public CommandResponse execute() {
                 var num = 1 / 0;
-                return new CommandResponse("Hello World!");
+                return new CommandResponse("Hello World!", TRANSFER);
             }
         };
 
