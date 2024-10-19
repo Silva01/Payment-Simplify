d62f089a5d486a2fd8176b131025745c23dabcaa
silva01 <silva20102@gmail.com>
2024-10-19 15:56:33 -0300

    refactor: Added to return BadTransferException

--- src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/domain/Command.java
+++ src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/domain/Command.java
@@ -1,5 +1,7 @@
 package br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain;
 
-public interface Command <T, E extends Throwable> {
-    void execute(T t) throws E;
+import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.transfer.BadTransferException;
+
+public interface Command <T> {
+    T execute() throws BadTransferException;
 }
