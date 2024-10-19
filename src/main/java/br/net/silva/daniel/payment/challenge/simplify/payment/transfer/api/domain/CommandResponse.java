bbebef3670b3f37a4f6865dbe7467372990839d3
silva01 <silva20102@gmail.com>
2024-10-19 20:12:02 -0300

    refactor: Refactored to use CommandResponseFluxEnum

--- src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/domain/CommandResponse.java
+++ src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/domain/CommandResponse.java
@@ -1,7 +1,8 @@
 package br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain;
 
 public record CommandResponse(
-        Object response
+        Object response,
+        CommandResponseFluxEnum flux
 ) {
 
     public <T> T response(Class<T> type) {
