f39f304d39f63ac91bb91b7fc0453134be17e758
silva01 <silva20102@gmail.com>
2024-10-19 17:42:44 -0300

    refactor: Refactored to use the object CommandResponse for response

--- src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/domain/CommandInvoker.java
+++ src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/domain/CommandInvoker.java
@@ -2,19 +2,28 @@ package br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.doma
 
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.transfer.BadTransferException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CommandInvoker {
 
-    private final Command<?> command;
+    private final List<Command> commands;
 
-    private CommandInvoker(Command<?> command) {
-        this.command = command;
+    public CommandInvoker() {
+        this.commands = new ArrayList<>();
     }
 
-    public static <T> CommandInvoker of (Command<T> command) {
-        return new CommandInvoker(command);
+    public CommandInvoker add(Command command) {
+        commands.add(command);
+        return this;
     }
 
-    public Object start() throws BadTransferException {
-        return command.execute();
+    public List<CommandResponse> start() throws BadTransferException {
+        final List<CommandResponse> responses = new ArrayList<>();
+        for (Command command : commands) {
+            responses.add(command.execute());
+        }
+
+        return responses;
     }
 }
