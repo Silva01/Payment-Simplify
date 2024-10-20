313cfccf30e9d84ed33eda7af4ad7d374079f208
silva01 <silva20102@gmail.com>
2024-10-19 21:52:19 -0300

    refactor: Moved annotation Getter to class

--- src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/domain/transfer/BadTransferException.java
+++ src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/domain/transfer/BadTransferException.java
@@ -2,9 +2,9 @@ package br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.doma
 
 import lombok.Getter;
 
+@Getter
 public class BadTransferException extends Exception {
 
-    @Getter
     private final Integer codError;
 
     public BadTransferException(String message, Integer codError) {
