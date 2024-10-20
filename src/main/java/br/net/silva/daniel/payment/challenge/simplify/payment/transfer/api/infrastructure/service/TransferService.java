d3ec71f844f70d9ff245f566543b5bdc4ce31b40
silva01 <silva20102@gmail.com>
2024-10-19 21:42:04 -0300

    refactor: Refactored method transferValue

--- src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/service/TransferService.java
+++ src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/service/TransferService.java
@@ -1,11 +1,17 @@
 package br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.service;
 
+import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.CommandInvoker;
+import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.CommandResponse;
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.transfer.BadTransferException;
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.transfer.TransferRequest;
 import org.springframework.stereotype.Service;
 
+import static br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.CommandResponseFluxEnum.TRANSFER;
+
 @Service
 public class TransferService {
 
-    public void transferValue(TransferRequest request) throws BadTransferException {}
+    public void transferValue(TransferRequest request) throws BadTransferException {
+        CommandInvoker.of().add(() -> new CommandResponse("Hello Word", TRANSFER)).start();
+    }
 }
