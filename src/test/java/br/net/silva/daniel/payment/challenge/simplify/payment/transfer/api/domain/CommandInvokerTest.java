3f2f665d25d6e6d5193b62c4a08dac0ae9632e9c
silva01 <silva20102@gmail.com>
2024-10-22 05:57:06 -0300

    refactor: #15 Refactored test to new scenario

--- src/test/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/domain/CommandInvokerTest.java
+++ src/test/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/domain/CommandInvokerTest.java
@@ -1,12 +1,13 @@
 package br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain;
 
+import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.commons.DummyErrorCommand;
+import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.commons.DummyIntegerCommand;
+import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.commons.DummyStringCommand;
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.transfer.BadTransferException;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.CommandResponseFluxEnum.EXTRACT;
-import static br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.CommandResponseFluxEnum.TRANSFER;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -15,100 +16,74 @@ class CommandInvokerTest {
     @Test
     void executionCommand_WithoutErrors() throws BadTransferException {
         // Arrange
-        final var command = new Command() {
-            @Override
-            public CommandResponse execute() {
-                return new CommandResponse("Hello World!", TRANSFER);
-            }
-        };
+        final var command = new DummyStringCommand();
 
         // Act
-        final var result = new CommandInvoker().add(command)
+        new CommandInvoker().add(command)
                 .start();
 
         // Assert
-        assertThat(result).isNotNull();
-        assertThat(result.responses()).hasSize(1);
-        assertThat(result.getResponseForFlux(TRANSFER).response(String.class)).isEqualTo("Hello World!");
+        assertThat(command.getMessage()).isNotNull();
+        assertThat(command.getMessage()).isEqualTo("Hello World!");
     }
 
     @Test
     void executionCommand_ProcessWithMoreTwoResponse_WithoutErrors() throws BadTransferException {
         // Arrange
-        final var command1 = new Command() {
-            @Override
-            public CommandResponse execute() {
-                return new CommandResponse("Hello World!", TRANSFER);
-            }
-        };
+        final var command1 = new DummyStringCommand();
 
-        final var command2 = new Command() {
-            @Override
-            public CommandResponse execute() {
-                return new CommandResponse(999, EXTRACT);
-            }
-        };
+        final var command2 = new DummyIntegerCommand();
 
         final var commandInvoker = new CommandInvoker();
         commandInvoker.add(command1);
         commandInvoker.add(command2);
+
         // Act
-        final var result = commandInvoker.start();
+        commandInvoker.start();
 
         // Assert
-        assertThat(result).isNotNull();
-        assertThat(result.responses()).hasSize(2);
-        assertThat(result.getResponseForFlux(TRANSFER).response(String.class)).isEqualTo("Hello World!");
-        assertThat(result.getResponseForFlux(EXTRACT).response(Integer.class)).isEqualTo(999);
+        assertThat(command1.getMessage()).isNotNull();
+        assertThat(command2.getCount()).isNotNull();
+        assertThat(command1.getMessage()).isEqualTo("Hello World!");
+        assertThat(command2.getCount()).isEqualTo(100);
     }
 
     @Test
-    void executionCommand_WithErrors() throws BadTransferException {
+    void executionCommand_WithErrors() {
         // Arrange
-        final var command = new Command() {
-            @Override
-            public CommandResponse execute() {
-                var num = 1 / 0;
-                return new CommandResponse("Hello World!", TRANSFER);
-            }
-        };
+        final var command = new DummyErrorCommand();
 
         // Act
         assertThatThrownBy(() -> new CommandInvoker().add(command).start())
-                .isInstanceOf(ArithmeticException.class);
+                .isInstanceOf(BadTransferException.class);
     }
 
     @Test
     void executionCommand_ProcessCreatedWithFactory_WithoutErrors() throws BadTransferException {
         // Arrange
+
+        final var command1 = new DummyStringCommand();
+        final var command2 = new DummyIntegerCommand();
+
         final var commandFactory = new CompositeCommandFactory() {
             @Override
             public List<Command> create() {
                 return List.of(
-                        new Command() {
-                            @Override
-                            public CommandResponse execute() {
-                                return new CommandResponse("Hello World!", TRANSFER);
-                            }
-                        },
-                        new Command() {
-                            @Override
-                            public CommandResponse execute() {
-                                return new CommandResponse(999, EXTRACT);
-                            }
-                        }
+                        command1,
+                        command2
                 );
             }
         };
 
         final var commandInvoker = new CommandInvoker().add(commandFactory);
+
         // Act
-        final var result = commandInvoker.start();
+        commandInvoker.start();
 
         // Assert
-        assertThat(result).isNotNull();
-        assertThat(result.responses()).hasSize(2);
-        assertThat(result.getResponseForFlux(TRANSFER).response(String.class)).isEqualTo("Hello World!");
-        assertThat(result.getResponseForFlux(EXTRACT).response(Integer.class)).isEqualTo(999);
+        assertThat(command1.getMessage()).isNotNull();
+        assertThat(command2.getCount()).isNotNull();
+        assertThat(command1.getMessage()).isEqualTo("Hello World!");
+        assertThat(command2.getCount()).isEqualTo(100);
     }
 }
