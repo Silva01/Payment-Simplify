c382faa345e8189810c74f4a835cbb5f66c21d73
silva01 <silva20102@gmail.com>
2024-10-22 07:06:23 -0300

    refactor: #15 Refactored to use validations in transfer method

--- src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/service/TransferService.java
+++ src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/service/TransferService.java
@@ -1,14 +1,29 @@
 package br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.service;
 
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.CommandInvoker;
+import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.Validate;
+import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.account.Account;
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.transfer.BadTransferException;
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.transfer.TransferRequest;
+import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.annotation.TransferAccountValidate;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@AllArgsConstructor
 public class TransferService {
 
+    @TransferAccountValidate
+    private final List<Validate<Account>> transferAccountValidates;
+
     public void transferValue(TransferRequest request) throws BadTransferException {
+
+        for (Validate<Account> validate : transferAccountValidates) {
+            validate.validate(Account.accountNotFound());
+        }
+
         CommandInvoker.of().add(() -> System.out.println("Opa")).start();
     }
 }
