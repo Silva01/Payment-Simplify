46964d2d4f57489e4a7679eb72f34c0a5b9dcd80
silva01 <silva20102@gmail.com>
2024-10-22 06:11:15 -0300

    refactor: #15 Added new constant to account not found

--- src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/domain/account/TypeAccountEnum.java
+++ src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/domain/account/TypeAccountEnum.java
@@ -1,5 +1,5 @@
 package br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.account;
 
 public enum TypeAccountEnum {
-    RETAILER_ACCOUNT, SIMPLE_ACCOUNT
+    RETAILER_ACCOUNT, SIMPLE_ACCOUNT, NOT_FOUND_ACCOUNT
 }
