6d548621bbafc7716dfc35f2f0cc02c89a9ca06a
silva01 <silva20102@gmail.com>
2024-10-22 05:57:59 -0300

    refactor: #15 Refactored to not break the testes

--- src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/service/TransferService.java
+++ src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/infrastructure/service/TransferService.java
@@ -1,17 +1,14 @@
 package br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.infrastructure.service;
 
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.CommandInvoker;
-import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.CommandResponse;
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.transfer.BadTransferException;
 import br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.transfer.TransferRequest;
 import org.springframework.stereotype.Service;
-
-import static br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.CommandResponseFluxEnum.TRANSFER;
 
 @Service
 public class TransferService {
 
     public void transferValue(TransferRequest request) throws BadTransferException {
-        CommandInvoker.of().add(() -> new CommandResponse("Hello Word", TRANSFER)).start();
+        CommandInvoker.of().add(() -> System.out.println("Opa")).start();
     }
 }
