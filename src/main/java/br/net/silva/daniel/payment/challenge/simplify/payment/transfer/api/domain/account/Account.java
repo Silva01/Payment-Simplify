3ab70c04ec575fc413836cafd7e19da46c0ee8f8
silva01 <silva20102@gmail.com>
2024-10-22 06:11:47 -0300

    refactor: #15 Added method to create account where account not exists in database

--- src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/domain/account/Account.java
+++ src/main/java/br/net/silva/daniel/payment/challenge/simplify/payment/transfer/api/domain/account/Account.java
@@ -8,8 +8,10 @@ import org.springframework.http.HttpStatus;
 
 import java.math.BigDecimal;
 
+import static br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.account.TypeAccountEnum.NOT_FOUND_ACCOUNT;
 import static br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.account.TypeAccountEnum.RETAILER_ACCOUNT;
 import static br.net.silva.daniel.payment.challenge.simplify.payment.transfer.api.domain.account.TypeAccountEnum.SIMPLE_ACCOUNT;
+import static java.util.Objects.isNull;
 
 @ToString
 @AllArgsConstructor
@@ -30,6 +32,10 @@ public class Account implements Aggregate {
         return new Account(cpf, name, email, password, BigDecimal.valueOf(1000), RETAILER_ACCOUNT);
     }
 
+    public static Account accountNotFound() {
+        return new Account(null, null, null, null, BigDecimal.ZERO, NOT_FOUND_ACCOUNT);
+    }
+
     public void validateAccountBalance(BigDecimal value) throws BadTransferException {
         if (balance.compareTo(value) < 0) {
             throw new BadTransferException("Balance insufficient for transfer", HttpStatus.FORBIDDEN.value());
@@ -38,5 +44,11 @@ public class Account implements Aggregate {
 
     public boolean isRetailer() {
         return RETAILER_ACCOUNT.equals(type);
+    }
+
+    public void exists() throws BadTransferException {
+        if (isNull(type) || NOT_FOUND_ACCOUNT.equals(type)) {
+            throw new BadTransferException("Account not found", HttpStatus.NOT_FOUND.value());
+        }
     }
 }
